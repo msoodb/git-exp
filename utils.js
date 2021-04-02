cc27ca3f7dabf06d879abdc5cdf6b51ced53bea0
msoodb <masoud.bolhassani@gmail.com>
2021-04-02 12:40:50 +0300

    faq and gitignore

---
+++ utils.js
@@ -0,0 +1,12 @@
+// TODO: Implement utils
+func datetotime() {
+  conxole.log('convert dato to time')
+}
+
+func timetodate() {
+  conxole.log('convert time to date')
+}
+
+func time() {
+  conxole.log('convert time')
+}
